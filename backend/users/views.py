dd5e917d39d40fd0c540028eaf11cb94739bd10c
Yoogi-7 <epssos@gmail.com>
2025-06-15 10:47:47 +0200

    Add user profile endpoint with client info

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import User
 from .serializers import UserSerializer
-
+from clients.models import Client
+from clients.serializers import ClientSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def profile(self, request):
+        user = request.user
+        data = UserSerializer(user).data
+        if user.role == 'client' and user.client:
+            client_data = ClientSerializer(user.client).data
+            data['client_info'] = client_data
+        return Response(data)
